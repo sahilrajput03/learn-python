59f1049f61f1e715bbbc8bfbd2d52cb76cebb3a7
Sahil Rajput <sahilrajput03@gmail.com>
2025-03-25 03:48:42 +0530

    Update.

--- 30-days-python-asabeneh/day_14/ex3.py
+++ 30-days-python-asabeneh/day_14/ex3.py
@@ -3,9 +3,14 @@ from utils import readCountriesFromJsonFile
 # Level 3: Sort countries by name, by capital, by population
 countries = readCountriesFromJsonFile()
 sorted_by_name = sorted(countries, key=lambda x: x["name"])
-# sorted_by_capital = sorted(countries, key=lambda x: x.get("capital", ""))
-# sorted_by_population = sorted(countries, key=lambda x: x.get("population", 0))
+sorted_by_capital = sorted(countries, key=lambda x: x.get("capital", ""))
+sorted_by_population = sorted(countries, key=lambda x: x.get("population", 0))
 
-print("Countries sorted by name:", [c["name"] for c in sorted_by_name[:5]])
-# print("Countries sorted by capital:", [c["name"] for c in sorted_by_capital[:5]])
-# print("Countries sorted by population:", [c["name"] for c in sorted_by_population[:5]])
+print("Countries sorted by name:")
+print([country["name"] for country in sorted_by_name[:5]])
+
+print("\nCountries sorted by capital:")
+print([country["name"] for country in sorted_by_capital[:5]])
+
+print("\nCountries sorted by population:")
+print([country["name"] for country in sorted_by_population[:5]])
