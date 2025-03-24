1b008e27c9f4d997761b01207b6a38020f4e1109
Sahil Rajput <sahilrajput03@gmail.com>
2025-03-25 02:52:24 +0530

    Update.

--- 30-days-python-asabeneh/day_14/ex1.py
+++ 30-days-python-asabeneh/day_14/ex1.py
@@ -1,11 +1,10 @@
+from functools import reduce
+
 countries = ["Estonia", "Finland", "Sweden", "Denmark", "Norway", "Iceland"]
 names = ["Asabeneh", "Lidiya", "Ermias", "Abraham"]
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# Exercises: Level 1
-#       Use for loop to print each country in the countries list.
-
-# Use map to create a new list by changing each country to uppercase in the
+# Ex. Use map to create a new list by changing each country to uppercase in the
 #    countries list
 
 uppercase_countries = map(lambda v: v.upper(), countries)
@@ -13,8 +12,39 @@ uppercase_countries = map(lambda v: v.upper(), countries)
 print(list(uppercase_countries))
 
 
-# Use filter to filter out countries containing 'land'.
+# Ex. Use filter to filter out countries containing 'land'.
 print(list(filter(lambda text: text.find("land") != -1, countries)))
 
-# Use filter to filter out countries having exactly six characters.
+# Ex. Use filter to filter out countries having exactly six characters.
 print(list(filter(lambda country: len(country) == 6, countries)))
+
+# Ex. Use filter to filter out countries containing six letters and more in the country list.
+print(list(filter(lambda country: len(country) > 6, countries)))
+
+# Ex. Use filter to filter out countries starting with an 'E'
+print(list(filter(lambda country: country.startswith("E"), countries)))
+
+
+# Ex. Declare a function called get_string_lists which takes a list as a
+# parameter and then returns a list containing only string items.
+def get_string_lists(list1):
+    return list(filter(lambda x: type(x) == str, list1))
+
+
+list1 = ["1", 2, 3, "4", 5, "6"]
+print(get_string_lists(list1))  # Output: ["1", "4", "6"]
+
+
+# Ex. Use reduce to concatenate all the countries and to produce this sentence:
+#       Estonia, Finland, Sweden, Denmark, Norway, and Iceland are north European
+#       countries
+
+
+def joinCountriesProperly(accumulator, current):
+    isLastElement = current == countries[-1]
+    if isLastElement:
+        return accumulator + ", and " + current
+    return accumulator + ", " + current
+
+
+print(reduce(joinCountriesProperly, countries) + " are north European countries")
