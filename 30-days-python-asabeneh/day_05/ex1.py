c507c7339479b1706c0559d4f3ce8f89ab2f43f9
Sahil Rajput <sahilrajput03@gmail.com>
2025-03-25 17:42:31 +0530

    Update.

--- 30-days-python-asabeneh/day_05/ex1.py
+++ 30-days-python-asabeneh/day_05/ex1.py
@@ -12,5 +12,14 @@ lst = ["item1", "item2", "item3", "item4", "item5"]
 lst[0] = "item101"
 print("\n", lst)
 
-# TODO: ex12: Insert an IT company in the middle of the companies list
+# ex12: Insert an IT company in the middle of the companies list
+it_companies = ["twitter", "facebook"]
+it_companies.insert(int(len(it_companies) / 2), "😍google")
+print(it_companies)
+
+# ex.13: Change one of the it_companies names to uppercase (IBM excluded!)
+it_companies[1] = it_companies[1].upper()
+print(it_companies)
+
+# TODO: continue from ex.14
 # https://github.com/Asabeneh/30-Days-Of-Python/blob/master/05_Day_Lists/05_lists.md
