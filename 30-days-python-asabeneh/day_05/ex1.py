ab46df6491012da441f8dfeee31455c5ead5fe41
Sahil Rajput <sahilrajput03@gmail.com>
2025-03-25 17:24:22 +0530

    Update.

---
+++ 30-days-python-asabeneh/day_05/ex1.py
@@ -0,0 +1,16 @@
+# ex4: Get the first item, the middle item and the last item of the list
+lst = ["Asabeneh", 250, True, {"country": "Finland", "city": "Helsinki"}, False]
+print(lst[0])  # first item
+print(lst[int((len(lst) + 1) / 2)])  # middle item
+print(lst[len(lst) - 1])  # last item
+
+print("\n", lst)
+
+
+# ex10: Print the list after modifying one of the companies
+lst = ["item1", "item2", "item3", "item4", "item5"]
+lst[0] = "item101"
+print("\n", lst)
+
+# TODO: ex12: Insert an IT company in the middle of the companies list
+# https://github.com/Asabeneh/30-Days-Of-Python/blob/master/05_Day_Lists/05_lists.md
