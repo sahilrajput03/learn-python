c0ffa5057c1f45cb7b115a743fe0c5f569cb7458
Sahil Rajput <sahilrajput03@gmail.com>
2025-03-25 19:39:48 +0530

    Update.

--- 30-days-python-asabeneh/day_14/ex3.py
+++ 30-days-python-asabeneh/day_14/ex3.py
@@ -13,4 +13,4 @@ print("\nCountries sorted by capital:")
 print([country["name"] for country in sorted_by_capital[:5]])
 
 print("\nCountries sorted by population:")
-print([country["name"] for country in sorted_by_population[:5]])
+print([country["name"] for country in sorted_by_population[:10]])
